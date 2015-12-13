bc8310bd686bdbbbfb3791757f64510d2e882073
WeichenXu <weichenxu1993@foxmail.com>
2015-12-12 22:01:52 -0500

    init

---
+++ WCX_hog.m
@@ -0,0 +1,8 @@
+% HOG according to Dalal, CVPR, 2005
+% Author: Weichen Xu, wx431@nyu.edu
+% Date: 12/12/2015
+% I: image window to extract hog feature
+% CellSize, BlockSize, Overlapping, 
+% Bin, Normal_Method
+% hogs: computed hog features
+function hogs = WCX_hog(I)
